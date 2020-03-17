2f411593cdf341fed1524171520d7a88bd01add7
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 18:44:22 +0530

    fix lint errors for python (except the header docstring)

--- actions/checkPrTitleForWIP.py
+++ actions/checkPrTitleForWIP.py
@@ -1,19 +1,30 @@
-import github, os, json
-from github import Github
+"""
+Github Action: comment on PRs that have WIP.
+"""
+
+import json
+import os
+
+import github
 
 def main():
-    gh = Github(os.getenv('GITHUB_TOKEN'))
+    """Triggers on PR edit/open/reopen."""
+
+    gh = github.Github(os.getenv('GITHUB_TOKEN'))
     event = json.load(open(os.getenv('GITHUB_EVENT_PATH')))
     if 'WIP' in event['pull_request']['title']:
         repo = gh.get_repo(event['repository']['full_name'])
         issue = repo.get_issue(event['pull_request']['number'])
-        comment = ('Hi @' + str(event['pull_request']['user']['login']) + ' '
+        comment = ('Hi @' + event['pull_request']['user']['login'] + ' '
                    'We typically do not want WIP PRs since each push will '
                    'make the Travis queue unnecessarily long. If you need '
                    'to run automated tests, please see our guides:'
                    'please see our guides: '
-                   'https://github.com/oppia/oppia/wiki/Setup-your-own-CircleCI-instance'
-                   'https://github.com/oppia/oppia/wiki/Setup-your-own-Travis-instance')
+                   'https://github.com/oppia/oppia/wiki/'
+                   'Setup-your-own-CircleCI-instance '
+                   'https://github.com/oppia/oppia/wiki/'
+                   'Setup-your-own-Travis-instance '
+                   'Thanks!')
         issue.create_comment(comment)
 
 if __name__ == '__main__':
